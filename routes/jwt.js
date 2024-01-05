d758f44cac9a06197e61ec4c6a4c2cdf47b62ef7
kostutarakesh <tarakesh.kostu@ro-one.in>
2024-01-05 22:06:22 +0530

    cyclic deploy

--- routes/jwt.js
+++ routes/jwt.js
@@ -1,11 +1,11 @@
 const jwt = require('jsonwebtoken');
 
 
-const secretKey = process.env.SECRET_KEY; 
+const secretKey = "SECRET_KEY";
 console.log(secretKey)
-  function generateToken(phoneNumber) {
-    return jwt.sign({ phoneNumber }, secretKey, { expiresIn: '1hr' }); // You can adjust the expiration time
-  }
+function generateToken(phoneNumber) {
+  return jwt.sign({ phoneNumber }, secretKey, { expiresIn: '1hr' }); // You can adjust the expiration time
+}
 
 
 function verifyToken(req, res, next) {
@@ -13,7 +13,7 @@ function verifyToken(req, res, next) {
     return res.status(400).json({ message: 'Bad Request' });
   }
   const tokenHeader = req.headers.authorization;
-console.log("tokengeader"+tokenHeader)
+  console.log("tokengeader" + tokenHeader)
   if (!tokenHeader) {
     return res.status(401).json({ message: 'Unauthorized' });
   }
@@ -21,9 +21,9 @@ console.log("tokengeader"+tokenHeader)
   const token = tokenHeader.split(' ')[1];
 
   jwt.verify(token, secretKey, (err, decoded) => {
-    console.log("Token:",token);
-    console.log("Secret key:",secretKey);
-    console.log("Decoded:",decoded);
+    console.log("Token:", token);
+    console.log("Secret key:", secretKey);
+    console.log("Decoded:", decoded);
 
     if (err) {
       console.error('JWT verification error:', err);
@@ -37,30 +37,30 @@ console.log("tokengeader"+tokenHeader)
 
 
 function generateRefreshToken(phoneNumber) {
-    return jwt.sign({ phoneNumber }, secretKey, { expiresIn: '7d' }); // Use the same secret key
-  }
+  return jwt.sign({ phoneNumber }, secretKey, { expiresIn: '7d' }); // Use the same secret key
+}
 
 // Handle token refresh
 function refreshToken(req, res) {
-    const refreshToken = req.body.refreshToken;
-    
-  
-    // Verify the refresh token
-    jwt.verify(refreshToken, secretKey, (err, decoded) => {
-        console.log("Refresh token:",refreshToken);
-        console.log("Secret key:",secretKey);
-        console.log("Decoded:",decoded);
-      if (err) {
-        console.error('Refresh token verification error:', err);
-        return res.status(403).json({ message: 'Forbidden' });
-      }
-  
-      // Generate a new access token
-      const newAccessToken = generateToken(decoded.phoneNumber);
-  
-      // Send the new access token to the client
-      res.json({ accessToken: newAccessToken });
-    });
-  }
-  
-  module.exports = { generateToken, verifyToken, refreshToken, generateRefreshToken };
+  const refreshToken = req.body.refreshToken;
+
+
+  // Verify the refresh token
+  jwt.verify(refreshToken, secretKey, (err, decoded) => {
+    console.log("Refresh token:", refreshToken);
+    console.log("Secret key:", secretKey);
+    console.log("Decoded:", decoded);
+    if (err) {
+      console.error('Refresh token verification error:', err);
+      return res.status(403).json({ message: 'Forbidden' });
+    }
+
+    // Generate a new access token
+    const newAccessToken = generateToken(decoded.phoneNumber);
+
+    // Send the new access token to the client
+    res.json({ accessToken: newAccessToken });
+  });
+}
+
+module.exports = { generateToken, verifyToken, refreshToken, generateRefreshToken };
