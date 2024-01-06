a75570b89489c993475ab8e5b8b46bba1e778b59
kostutarakesh <tarakesh.kostu@ro-one.in>
2024-01-06 23:16:28 +0530

    cyclic deploy

--- app.js
+++ app.js
@@ -18,19 +18,30 @@ app.use(cors());
 
 // MongoDB connection URL
 // const dbUrl = "mongodb+srv://KomarajuBablu:Honeybablu1772@cluster0.zktypoh.mongodb.net/VIPMEAPP?retryWrites=true&w=majority";
-const dbUrl = "mongodb+srv://tarakeshkostu:tarakeshkostu@cluster0.oklqzax.mongodb.net/?retryWrites=true&w=majority"
-  // "mongodb+srv://KomarajuBablu:Honeybablu1772@cluster0.zktypoh.mongodb.net/VIPMEAPP?retryWrites=true&w=majority";
-// Connecting to MongoDB
-const connectToDatabase = async () => {
-  try {
-    await mongoose.connect(dbUrl);
-    console.log("Connected to MongoDB");
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-  }
-};
+// const dbUrl = "mongodb+srv://tarakeshkostu:tarakeshkostu@cluster0.oklqzax.mongodb.net/?retryWrites=true&w=majority"
+//   // "mongodb+srv://KomarajuBablu:Honeybablu1772@cluster0.zktypoh.mongodb.net/VIPMEAPP?retryWrites=true&w=majority";
+// // Connecting to MongoDB
+// const connectToDatabase = async () => {
+//   try {
+//     await mongoose.connect(dbUrl);
+//     console.log("Connected to MongoDB");
+//   } catch (error) {
+//     console.error("Error connecting to MongoDB:", error);
+//   }
+// };
 
-connectToDatabase();
+// connectToDatabase();
+
+mongoose.connect('mongodb+srv://tarakeshkostu:tarakeshkostu@cluster0.oklqzax.mongodb.net/?retryWrites=true&w=majority', console.log("Dbconnected"))
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Header', 'Origin, X-Requested-With ,Content-Type,Accept,Autherization');
+  if (req.method == 'Options') {
+    res.header('Access-Control-Allow-Origin', 'PUT,POST,GET,DELETE,PATCH');
+    return res.status(200).json({});
+  }
+  next();
+});
 
 // app.listen(port, () => {
 //   console.log(`Server is Running at: http://localhost:${port}`);
